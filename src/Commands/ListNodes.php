ec7068f13be4ac918d1cea40d91fbfb8ba9d6f5a
Jon West <jowest@corelogic.com>
2017-07-11 17:52:30 -0600

    Adding try catch and response validation

--- src/Commands/ListNodes.php
+++ src/Commands/ListNodes.php
@@ -31,43 +31,49 @@ class ListNodes extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $url = $input->getArgument('url');
-        $node = $input->getArgument('node');
-        $client = new Client();
+        try {
+            $url = $input->getArgument('url');
+            $node = $input->getArgument('node');
+            $client = new Client();
 
-        $response = $client->request('GET', $url);
+            $response = $client->request('GET', $url);
+            if ($response->getStatusCode() > 300) {
+                throw new \Exception("Guzzle request returned: " . $response->getReasonPhrase());
+            }
+            $crawler = new Crawler((string)$response->getBody());
+            $attributes = explode(',', $input->getArgument('attr'));
 
-        $crawler = new Crawler((string)$response->getBody());
-        $attributes = explode(',', $input->getArgument('attr'));
+            switch ($input->getOption('format')) {
+                case 'json':
+                    $outputFormat = new JSONFormat($output);
+                    break;
+                case 'link':
+                    $outputFormat = new LinkFormat($output);
+                    break;
+                case 'table':
+                default:
+                    $outputFormat = new TableFormat($output);
+                    break;
+            }
 
-        switch ($input->getOption('format')) {
-            case 'json':
-                $outputFormat = new JSONFormat($output);
-                break;
-            case 'link':
-                $outputFormat = new LinkFormat($output);
-                break;
-            case 'table':
-            default:
-                $outputFormat = new TableFormat($output);
-                break;
+            $rows = [];
+            $crawler
+                ->filter($node)
+                ->each(function (Crawler $element, $i) use ($output, $attributes, &$rows) {
+                    if ($output->getVerbosity() > OutputInterface::VERBOSITY_VERBOSE) {
+                        $output->writeln(['element' => $element->html(), 'index' => $i]);
+                    }
+                    $row = [];
+                    $row['text'] = $element->text();
+                    foreach ($attributes as $attr) {
+                        $row[$attr] = $element->attr($attr);
+                    }
+                    $rows[] = $row;
+                });
+
+            $outputFormat->respond($rows);
+        } catch (\Exception $exception) {
+            $output->write("Exception caught: " . $exception->getMessage() . "\n");
         }
-
-        $rows = [];
-        $crawler
-            ->filter($node)
-            ->each(function (Crawler $element, $i) use ($output, $attributes, &$rows) {
-                if ($output->getVerbosity() > OutputInterface::VERBOSITY_VERBOSE) {
-                    $output->writeln(['element' => $element->html(), 'index' => $i]);
-                }
-                $row = [];
-                $row['text'] = $element->text();
-                foreach ($attributes as $attr) {
-                    $row[$attr] = $element->attr($attr);
-                }
-                $rows[] = $row;
-            });
-
-        $outputFormat->respond($rows);
     }
 }
